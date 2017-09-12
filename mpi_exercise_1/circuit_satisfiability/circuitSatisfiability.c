03d6f725a1285d8d3d0aef3edecea5dbd4f71811
Mitchell Stark <mjs73@dijkstra.cs.calvin.edu>
2017-09-11 23:49:53 -0400

    fix final print statement bug

--- mpi_exercise_1/circuit_satisfiability/circuitSatisfiability.c
+++ mpi_exercise_1/circuit_satisfiability/circuitSatisfiability.c
@@ -16,29 +16,31 @@
 int checkCircuit (int, long);
 
 int main (int argc, char *argv[]) {
-   long i;               /* loop variable (64 bits) */
-   int id = -1, numProcesses = -1;           /* process id */
-   int count = 0;        /* number of solutions */
-   int totalCount = 0;
-   double startTime = 0.0, totalTime = 0.0;
-   startTime = MPI_Wtime();
+	long i;               /* loop variable (64 bits) */
+	int id = -1, numProcesses = -1;           /* process id */
+	int count = 0;        /* number of solutions */
+	int totalCount = 0;
+	double startTime = 0.0, totalTime = 0.0;
+	startTime = MPI_Wtime();
 
-   MPI_Init(&argc, &argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &id);
-   MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-   for (i = id; i <= UINT_MAX; i += numProcesses) {
-      count += checkCircuit (id, i);
-   }
+	for (i = id; i <= UINT_MAX; i += numProcesses) {
+		count += checkCircuit (id, i);
+	}
 
-   totalTime = MPI_Wtime() - startTime;
+	totalTime = MPI_Wtime() - startTime;
 
-   printf ("Process %d finished in time %f secs.\n", id, totalTime);
-   fflush (stdout);
-   MPI_Reduce(&count, &totalCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-   printf("\nA total of %d solutions were found.\n\n", totalCount);
-   MPI_Finalize();
-   return 0;
+	printf ("Process %d finished in time %f secs.\n", id, totalTime);
+	fflush (stdout);
+	MPI_Reduce(&count, &totalCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	if (id == 0) {
+		printf("\nA total of %d solutions were found.\n\n", totalCount);
+	}
+	MPI_Finalize();
+	return 0;
 }
 
 /* EXTRACT_BIT is a macro that extracts the ith bit of number n.
@@ -63,40 +65,40 @@ int main (int argc, char *argv[]) {
 #define SIZE 32
 
 int checkCircuit (int id, long bits) {
-   int v[SIZE];        /* Each element is a bit of bits */
-   int i;
+	int v[SIZE];        /* Each element is a bit of bits */
+	int i;
 
-   for (i = 0; i < SIZE; i++)
-     v[i] = EXTRACT_BIT(bits,i);
+	for (i = 0; i < SIZE; i++)
+		v[i] = EXTRACT_BIT(bits,i);
 
-   if ( ( (v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
-       && (!v[3] || !v[4]) && (v[4] || !v[5])
-       && (v[5] || !v[6]) && (v[5] || v[6])
-       && (v[6] || !v[15]) && (v[7] || !v[8])
-       && (!v[7] || !v[13]) && (v[8] || v[9])
-       && (v[8] || !v[9]) && (!v[9] || !v[10])
-       && (v[9] || v[11]) && (v[10] || v[11])
-       && (v[12] || v[13]) && (v[13] || !v[14])
-       && (v[14] || v[15]) )
-       ||
-          ( (v[16] || v[17]) && (!v[17] || !v[19]) && (v[18] || v[19])
-       && (!v[19] || !v[20]) && (v[20] || !v[21])
-       && (v[21] || !v[22]) && (v[21] || v[22])
-       && (v[22] || !v[31]) && (v[23] || !v[24])
-       && (!v[23] || !v[29]) && (v[24] || v[25])
-       && (v[24] || !v[25]) && (!v[25] || !v[26])
-       && (v[25] || v[27]) && (v[26] || v[27])
-       && (v[28] || v[29]) && (v[29] || !v[30])
-       && (v[30] || v[31]) ) )
-   {
-      printf ("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d \n", id,
-         v[31],v[30],v[29],v[28],v[27],v[26],v[25],v[24],v[23],v[22],
-         v[21],v[20],v[19],v[18],v[17],v[16],v[15],v[14],v[13],v[12],
-         v[11],v[10],v[9],v[8],v[7],v[6],v[5],v[4],v[3],v[2],v[1],v[0]);
-      fflush (stdout);
-      return 1;
-   } else {
-      return 0;
-   }
+	if ( ( (v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
+				&& (!v[3] || !v[4]) && (v[4] || !v[5])
+				&& (v[5] || !v[6]) && (v[5] || v[6])
+				&& (v[6] || !v[15]) && (v[7] || !v[8])
+				&& (!v[7] || !v[13]) && (v[8] || v[9])
+				&& (v[8] || !v[9]) && (!v[9] || !v[10])
+				&& (v[9] || v[11]) && (v[10] || v[11])
+				&& (v[12] || v[13]) && (v[13] || !v[14])
+				&& (v[14] || v[15]) )
+			||
+			( (v[16] || v[17]) && (!v[17] || !v[19]) && (v[18] || v[19])
+			  && (!v[19] || !v[20]) && (v[20] || !v[21])
+			  && (v[21] || !v[22]) && (v[21] || v[22])
+			  && (v[22] || !v[31]) && (v[23] || !v[24])
+			  && (!v[23] || !v[29]) && (v[24] || v[25])
+			  && (v[24] || !v[25]) && (!v[25] || !v[26])
+			  && (v[25] || v[27]) && (v[26] || v[27])
+			  && (v[28] || v[29]) && (v[29] || !v[30])
+			  && (v[30] || v[31]) ) )
+	{
+		printf ("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d \n", id,
+				v[31],v[30],v[29],v[28],v[27],v[26],v[25],v[24],v[23],v[22],
+				v[21],v[20],v[19],v[18],v[17],v[16],v[15],v[14],v[13],v[12],
+				v[11],v[10],v[9],v[8],v[7],v[6],v[5],v[4],v[3],v[2],v[1],v[0]);
+		fflush (stdout);
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
