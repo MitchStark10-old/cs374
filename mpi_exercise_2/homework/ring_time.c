4893ff6da1b1bafc2ed6d691db3b925eabcdfde7
Mitchell Stark <mjs73@hopper.cs.calvin.edu>
2017-09-17 00:38:24 -0400

    timing working

--- mpi_exercise_2/homework/ring_time.c
+++ mpi_exercise_2/homework/ring_time.c
@@ -13,6 +13,7 @@ int odd(int number) { return number % 2; }
 
 int main(int argc, char** argv) {
     int id = -1, numProcesses = -1; 
+    double startTime = -1, endTime = -1;
     int sendAddress = -1;
     MPI_Status status;
 
@@ -29,6 +30,7 @@ int main(int argc, char** argv) {
         MPI_Finalize();
         return 0;
     }
+    startTime = MPI_Wtime();
 
     sendString = (char*) malloc( SIZE );
     receivedString = (char*) malloc( SIZE );
@@ -40,7 +42,9 @@ int main(int argc, char** argv) {
         //Recieve a message from n-1 process
         MPI_Recv(receivedString, SIZE, MPI_CHAR, numProcesses - 1, 1, MPI_COMM_WORLD, &status);
         //Print the message and time
+        endTime = MPI_Wtime();
         printf("%s\n", receivedString);
+        printf("Time: %f\n", endTime - startTime);
     } else {
         //Recieve message from rank i - 1
         MPI_Recv(receivedString, SIZE, MPI_CHAR, id - 1, 1, MPI_COMM_WORLD, &status);
