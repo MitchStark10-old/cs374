3b90abcbca681e96f550650a7a81e57f00999fa6
Mitchell Stark <mjs73@codd.cs.calvin.edu>
2017-11-05 20:53:10 -0500

    mpi code completed

--- exercise_7/project/mpiScatterSum.c
+++ exercise_7/project/mpiScatterSum.c
@@ -1,34 +1,67 @@
 /* arraySum.c uses an array to sum the values in an input file,
  *  whose name is specified on the command-line.
  * Joel Adams, Fall 2005
+ * Mitch Stark, Fall 2017
+ * Updates made to parallelize the array sum with MPI
  * for CS 374 (HPC) at Calvin College.
  */
 
 #include <stdio.h>      /* I/O stuff */
 #include <stdlib.h>     /* calloc, etc. */
+#include "mpi.h"
 
 void readArray(char * fileName, double ** a, int * n);
 double sumArray(double * a, int numValues) ;
 
 int main(int argc, char * argv[])
 {
-  int  howMany;
-  double sum;
-  double * a;
+    int  howMany;
+    int chunk_size;
+    int id;
+    int num_processes;
+    double * sub_a;
+    double partial_sum;
+    double total_sum;
+    double * a;
 
-  if (argc != 2) {
-    fprintf(stderr, "\n*** Usage: arraySum <inputFile>\n\n");
-    exit(1);
-  }
-  
-  readArray(argv[1], &a, &howMany);
-  sum = sumArray(a, howMany);
-  printf("The sum of the values in the input file '%s' is %g\n",
-           argv[1], sum);
+    if (argc != 2) {
+        fprintf(stderr, "\n*** Usage: arraySum <inputFile>\n\n");
+        exit(1);
+    }
 
-  free(a);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 
-  return 0;
+    if (id == 0) {
+        readArray(argv[1], &a, &howMany);
+    }
+    MPI_Bcast(&howMany, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    chunk_size = howMany / num_processes;
+    if (id == (num_processes - 1)) {
+        chunk_size += howMany % num_processes;
+    }
+
+    //send array to all the processes
+    sub_a = malloc(sizeof(double) * chunk_size);
+    MPI_Scatter(a, chunk_size, MPI_DOUBLE, sub_a, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+
+    partial_sum = sumArray(sub_a, chunk_size);
+
+    MPI_Reduce(&partial_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    MPI_Finalize();
+
+    if (id == 0) {
+        printf("The sum of the values in the input file '%s' is %g\n",
+                argv[1], total_sum);
+    }
+
+    free(sub_a);
+
+    return 0;
 }
 
 /* readArray fills an array with values from a file.
@@ -42,32 +75,32 @@ int main(int argc, char * argv[])
  */
 
 void readArray(char * fileName, double ** a, int * n) {
-  int count, howMany;
-  double * tempA;
-  FILE * fin;
+    int count, howMany;
+    double * tempA;
+    FILE * fin;
 
-  fin = fopen(fileName, "r");
-  if (fin == NULL) {
-    fprintf(stderr, "\n*** Unable to open input file '%s'\n\n",
-                     fileName);
-    exit(1);
-  }
+    fin = fopen(fileName, "r");
+    if (fin == NULL) {
+        fprintf(stderr, "\n*** Unable to open input file '%s'\n\n",
+                fileName);
+        exit(1);
+    }
 
-  fscanf(fin, "%d", &howMany);
-  tempA = calloc(howMany, sizeof(double));
-  if (tempA == NULL) {
-    fprintf(stderr, "\n*** Unable to allocate %d-length array",
-                     howMany);
-    exit(1);
-  }
+    fscanf(fin, "%d", &howMany);
+    tempA = calloc(howMany, sizeof(double));
+    if (tempA == NULL) {
+        fprintf(stderr, "\n*** Unable to allocate %d-length array",
+                howMany);
+        exit(1);
+    }
 
-  for (count = 0; count < howMany; count++)
-   fscanf(fin, "%lf", &tempA[count]);
+    for (count = 0; count < howMany; count++)
+        fscanf(fin, "%lf", &tempA[count]);
 
-  fclose(fin);
+    fclose(fin);
 
-  *n = howMany;
-  *a = tempA;
+    *n = howMany;
+    *a = tempA;
 }
 
 /* sumArray sums the values in an array of doubles.
@@ -77,14 +110,14 @@ void readArray(char * fileName, double ** a, int * n) {
  */
 
 double sumArray(double * a, int numValues) {
-  int i;
-  double result = 0.0;
+    int i;
+    double result = 0.0;
 
-  for (i = 0; i < numValues; i++) {
-    result += *a;
-    a++;
-  }
+    for (i = 0; i < numValues; i++) {
+        result += *a;
+        a++;
+    }
 
-  return result;
+    return result;
 }
 
