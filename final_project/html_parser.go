bd89d79b8820b51709040fe59af36d889acf7798
MitchStark10 <MitchStark10@gmail.com>
2017-11-10 11:44:31 -0500

    change to schedule page

--- final_project/html_parser.go
+++ final_project/html_parser.go
@@ -8,11 +8,11 @@ import (
 
 func getSiteList() [5]string {
 	var sites [5]string;
-	sites[0] = "http://calvinknights.com/sports/msoc/2017-18/teams/calvin?view=profile&r=0&pos=kickers";
-	sites[1] = "http://athletics.hope.edu/sports/msoc/2017-18/teams/hope?view=profile&r=0&pos=kickers";
-	sites[2] = "http://hornets.kzoo.edu/sports/msoc/2017-18/teams/kalamazoo?view=profile&r=0&pos=kickers";
-	sites[3] = "http://www.adrianbulldogs.com/sports/m-soccer/2017-18/teams/adrian?view=profile&r=0&pos=kickers";
-	sites[4] = "http://www.gobrits.com/sports/msoc/2017-18/teams/albion?view=profile&r=0&pos=kickers";
+	sites[0] = "http://calvinknights.com/sports/msoc/2017-18/schedule" ;
+	sites[1] = "http://athletics.hope.edu/sports/msoc/2017-18/schedule";
+	sites[2] = "http://hornets.kzoo.edu/sports/msoc/2017-18/schedule";
+	sites[3] = "http://www.adrianbulldogs.com/sports/m-soccer/2017-18/schedule";
+	sites[4] = "http://www.gobrits.com/sports/msoc/2017-18/schedule";
 
 	return sites;
 }
