36bcb583fe29ba794fcdf99dda57d284dc350eae
MitchStark10 <MitchStark10@gmail.com>
2017-11-14 20:01:09 -0500

    python scraper working

--- final_project/python_work/web_scraper.py
+++ final_project/python_work/web_scraper.py
@@ -11,13 +11,42 @@ def getSiteList():
     sites.append("http://www.gobrits.com/sports/msoc/2017-18/schedule")
     return sites
 
+def getSoup(site):
+    page = urllib2.urlopen(site)
+    return BeautifulSoup(page, "html.parser")
 
 def getSchedule(site):
-    page = urllib2.urlopen(site)
-    page_soup = BeautifulSoup(page, "html.parser")
+    schedule = []
+    page_soup = getSoup(site)
     game_links = page_soup.find_all("a", href=re.compile("boxscores"))
-    print(game_links)
+    for link in game_links:
+        new_game = str(site).replace("/sports/msoc/2017-18/schedule", "").replace("/sports/m-soccer/2017-18/schedule", "") + link['href']
+        schedule.append(new_game)
+    return list(set(schedule))
+
+def getScore(game_link):
+    summary_str = ""
+    page_soup = getSoup(game_link)
+    line_score = page_soup.find("div", { "class" : "linescore" })
+    if line_score is None:
+        return
+
+    print(game_link)        
+    table_items = line_score.find_all("tr")
+
+    for item in [table_items[1], table_items[2]]:
+        team_name = item.find("th").contents[0].strip()
+        if team_name == "":
+            team_name = item.find("a").contents[0].strip()
+        summary_str += team_name + \
+            " - " + item.find("td", { "class" : "score total"} ).contents[0] + "\n"
+    
+    print(summary_str)
 
 site_list = getSiteList()
+schedule = []
 for site in site_list:
-    getSchedule(site)
+    schedule += getSchedule(site)
+
+for game_link in schedule:
+    getScore(game_link)
